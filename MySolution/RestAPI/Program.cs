08b2038571d98ec46785bc5438cf2a76e526626f
🐢 <tpham2580gmail.com>
2025-04-22 19:03:59 -0700

    Set up working dockerfiles with default RestAPI, Grpc, and Redis docker containers

--- MySolution/RestAPI/Program.cs
+++ MySolution/RestAPI/Program.cs
@@ -1,15 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    Console.WriteLine("This is the RestAPI");
 }
 
 app.UseHttpsRedirection();
@@ -21,7 +19,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
