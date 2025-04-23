35bf4b074af54ac465f5273bd6d7819003e61e63
🐢 <tpham2580gmail.com>
2025-04-22 20:29:47 -0700

    created Item Model

---
+++ MySolution/RestAPI/Models/Item.cs
@@ -0,0 +1,9 @@
+namespace RestAPI.Models
+{
+    public class Item
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+    }
+}
