24929384402e59dcfb107293a2e444b66c096261
🐢 <tpham2580gmail.com>
2025-04-22 20:43:40 -0700

    added Request and Response item objects

---
+++ MySolution/RestAPI/Dtos/ItemRequest.cs
@@ -0,0 +1,7 @@
+namespace RestAPI.Dtos;
+
+public class ItemRequest
+{
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
---
+++ MySolution/RestAPI/Dtos/ItemResponse.cs
@@ -0,0 +1,8 @@
+namespace RestAPI.Dtos;
+
+public class ItemResponse
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
