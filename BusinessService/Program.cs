0aa5604c6d168ef2a4292a9ce5a4096145da5716
🐢 <tpham2580@gmail.com>
2025-05-16 12:47:02 -0700

    added Log Enrichment for CallerInfo

--- BusinessService/Program.cs
+++ BusinessService/Program.cs
@@ -2,15 +2,24 @@ using BusinessService.Services;
 using BusinessService.Infrastructure;
 using BusinessService.Application;
 using Serilog;
+using Serilog.Enrichers.WithCaller;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Logging
 Log.Logger = new LoggerConfiguration()
-    .WriteTo.Console()
+    .Enrich.FromLogContext()
+    .Enrich.WithCaller(includeFileInfo: true)
+    .WriteTo.Console(
+        outputTemplate:
+        "[{Timestamp:yyyy-MM-dd HH:mm:ss} {Level:u3}] " +
+        "{SourceContext}.{CallerMemberName} :: {Message:lj}{NewLine}{Exception}")
     .WriteTo.File("Logs/business-log-.txt",
-             rollingInterval: RollingInterval.Day,
-             retainedFileCountLimit: 14)
+        rollingInterval: RollingInterval.Day,
+        retainedFileCountLimit: 14,
+        outputTemplate:
+        "[{Timestamp:yyyy-MM-dd HH:mm:ss} {Level:u3}] " +
+        "{SourceContext}.{CallerMemberName} :: {Message:lj}{NewLine}{Exception}")
     .CreateLogger();
 builder.Host.UseSerilog();
 
